6ca7e5e44529bb4fd8c7c0bb6cc9dee071e8b44f
junaidchauhdary <121387445+junaidchauhdary@users.noreply.github.com>
2023-02-09 11:01:50 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+
+let ham = document.querySelector("#hamburger");
+let nav = document.querySelector("#navbar");
+ham.addEventListener("click", function () {
+    nav.classList.toggle("active")
+}); 
